7f8159c326539e8df48a2330fc3af75709e71e3f
Lanang Agung <johnlanang@gmail.com>
2025-05-15 21:21:58 +0700

    Create rpg-maker-interview-form.php

---
+++ rpg-maker-interview-form.php
@@ -0,0 +1,12 @@
+/*
+Plugin Name: RPG Maker Interview Form
+Plugin URI: https://github.com/NAMA_KAMU/rpg-maker-interview-form
+Description: Plugin RPG Form dengan chapter & whitelist
+Version: 2.0.1
+Author: Lanang
+Author URI: https://tombolstart.com
+License: GPL2
+
+GitHub Plugin URI: https://github.com/NAMA_KAMU/rpg-maker-interview-form
+GitHub Branch: main
+*/
